07c08ff62ae3e46f93b0e3edb797ca91ea31dfa1
richie.p <mohammadpagard.dev@gmail.com>
2024-07-12 20:07:52 +0330

    improve -Wrote Limit algorithm & Wrote Top One algorithm.

---
+++ limit.py
@@ -0,0 +1,37 @@
+"""This algorithm return the minimum or maximum of your min or max choice
+    :param array: That array you're going to through on it.
+    :param min: The minimum integer you gave.
+    :param max: The maximum integer you gave.
+
+    It's through on array and returns items that you gave in min or max. e.g:
+
+    [1, 2, 3, 4, 5]
+        min 3   =>  [3, 4, 5]
+        max 3   =>  [1, 2, 3]
+        max, min 3  =>  [3]
+
+"""
+
+def limit_struct(array: list[int], min: int=None, max: int=None):
+    result = []
+
+    if (min != max) and (min or max):
+        for val in array:
+            if (min) and val >= min:
+                result.append(val)
+            elif (max) and val <= max:
+                result.append(val)
+    else:
+        result.append(min)
+
+    return result
+
+def limit_python(array: list[int], min: int=None, max: int=None):
+    """
+        Write a limit function with Python abilities.
+        -lambda, list comprehensions-
+    """
+
+    min_check = lambda val: True if min is None else (val >= min)
+    max_check = lambda val: True if max is None else (val <= max)
+    return [val for val in array if min_check(val) and max_check(val)]
---
+++ top_one.py
@@ -0,0 +1,28 @@
+"""Top one algorithm
+    :variable counter: This dictionary count the repeated elements in the array.
+    :variable max_reapeat: The value of this variable is the maximum repetition of the `counter`.
+    :variable result: The elements of this array save the repetitions of the `array` parameter.
+
+    e.g:
+    [1, 2, 1, 3, 4, 2, 2, 2]
+        counter = {'1': 2, '2': 4, '3': 1, '4': 1}
+        max_repeat = 4
+        result = [1, 2]
+"""
+
+def top_one(array: list) -> list:
+    result = []
+    counter = {}
+    max_repeat = 0
+
+    for val in array:
+        if val not in counter:
+            counter[val] = 1
+        else:
+            counter[val] += 1
+            if val not in result:
+                result.append(val)
+
+    max_repeat = max(counter.values())
+
+    return result, max_repeat
