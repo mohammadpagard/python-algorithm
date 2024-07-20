d01c1571802ea722da333b0d6a63744f79b0ce87
richie.p <mohammadpagard.dev@gmail.com>
2024-07-21 00:07:01 +0330

    fix -Fixed some grammer mistakes in sale and buy stock algorithm doc-string.

--- sell_buy_stock.py
+++ sell_buy_stock.py
@@ -1,13 +1,13 @@
 """
     You can find the best profit in the list.
-    This algorithm is easy but, attention to the formula.
+    This algorithm is easy, but pay attention to the formula.
 """
 
 
 def max_profit(array: list[int]):
     """
-    	curr_max: get current maximum between array indexes and zero and own.
-    	max_so_far: save the best one using get maximum of own and current max(curr_max).
+    	curr_max: get the current maximum between `array indexes` and `0` and `curr_max`.
+    	max_so_far: save the best one by getting the maximum of `max_so_far` and `curr_max`.
     """
     curr_max, max_so_far = 0, 0
     for i in range(1, len(array)):
@@ -15,6 +15,6 @@ def max_profit(array: list[int]):
         max_so_far = max(max_so_far, curr_max)
     return max_so_far
 
-#example
+# example
 print(max_profit([7, 1, 5, 3, 6, 4]))   # 5
 print(max_profit([9, 7, 6, 4, 3, 1]))   # 0
