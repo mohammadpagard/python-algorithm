99fe9aa76394fb350c1899463324a7f52f6ed701
richie.p <mohammadpagard.dev@gmail.com>
2024-07-28 15:35:26 +0330

    improve -Implement the deletion actions(delete at beginning and delete at end) in circular doubly linked list & fix -The display method pointer print is updated to: <-> for circular linked list.

--- data-structure/circular_doubly_linked_list.py
+++ data-structure/circular_doubly_linked_list.py
@@ -10,17 +10,17 @@ class Node:
         self.data = data
         self.next = None
         self.prev = None
-    
+
 
 class CircularDoublyLinkedList:
     """A class to represent a Circular Doubly Linked List.
     Attributes:
         - head(Node): The head/start node of the list.
     """
-    
+
     def __init__(self):
         self.head = None
-    
+
     def insert_begin(self, data):
         """Insert a new node at the beginning of the list.
         If the list is empty, then the new node is points to the `head` node of the list
@@ -69,6 +69,43 @@ class CircularDoublyLinkedList:
             new_node.next = self.head
             self.head.prev = new_node
 
+    def delete_begin(self):
+        """Delete the beginning node of the list
+        If the list contains only one node, it removes that node and sets the `head` to None.
+        Otherwise -list contains multiple nodes-, the new `head` node is set and the 
+        improtant pointers are updated.
+        """
+
+        if not self.head:
+            return "List is empty..."
+        if self.head.next == self.head:
+            print("Deleted {} ".format(self.head.data))
+            self.head = None
+        else:
+            tail = self.head.prev
+
+            tail.next = self.head.next    # last node points to the next of the `head` node
+            self.head.next.prev = tail  # new `head` node points to the `tail(last)` node
+            self.head = self.head.next  # the `head` node points to the next node of the `head`
+
+    def delete_end(self):
+        """Delete the beginning node of the list
+        If the list contains only one node, it removes that node and sets the `head` to None.
+        Otherwise -list contains multiple nodes-, the new `last` node is set and the 
+        improtant pointers are updated.
+        """
+
+        if not self.head:
+            return "List is empty..."
+        if self.head.next == self.head:
+            print("Deleted {} ".format(self.head.data))
+            self.head = None
+        else:
+            second_to_last = self.head.prev.prev    # this will update to the last node
+
+            second_to_last.next = self.head # next node of the `second to last` node points to `head` and updated to the `last node`
+            self.head.prev = second_to_last # the `head` node previous pointer is points to the `second to last (new last)` node
+
     def display(self):
         """Displays the nodes in the Circular Doubly Linked List
         """
@@ -85,8 +122,8 @@ class CircularDoublyLinkedList:
             if current == self.head:
                 break
 
-        result = ' -> '.join(str(data) for data in nodes)
-        print(result, '->', nodes[0])   # print result and last node point to first node
+        result = ' <-> '.join(str(data) for data in nodes)
+        print(result, '<->', nodes[0])   # print result and last node point to first node
 
 
 cdll = CircularDoublyLinkedList()
