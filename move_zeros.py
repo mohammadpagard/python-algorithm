1c3fcb65cea232a3ada74a2bf57b52268b2a47f0
richie.p <mohammadpagard.dev@gmail.com>
2024-08-01 19:21:59 +0330

    improve -Implement move zeros algorithm.

---
+++ move_zeros.py
@@ -0,0 +1,22 @@
+"""
+    Move Zeros
+    [0, False, 1, 0, 1, 2, 0, 1, 3, 'python', 0] => [False, 1, 1, 2, 1, 3, 'python', 0, 0, 0, 0]
+
+    move zeros to end of the list
+"""
+
+def move_zeros(array: list) -> list:
+    pointer = len(array) - 1
+    zero = 0
+
+    while pointer >= 0:
+        element = array[pointer]
+
+        if element == 0 and type(element) != bool:
+            array.pop(pointer)
+            zero += 1
+        pointer -= 1
+
+    array.extend([0] * zero)
+    return array
+
