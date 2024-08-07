cb28ab636529214b58d909daaf932c7b09b651ed
richie.p <mohammadpagard.dev@gmail.com>
2024-08-07 21:03:59 +0330

    improve -Implement the linear search algorithm.

---
+++ linear_search.py
@@ -0,0 +1,7 @@
+def linear_search(array: list[int], target: int) -> str:
+    for i in range(len(array)):
+        if array[i] == target:
+            return "The {} is in the {} index of the array".format(target, i)
+
+    return "Your target is not in the array..."
+
