09311cd1d8642becc39141b1aecda114d46ac020
richie.p <mohammadpagard.dev@gmail.com>
2024-08-07 21:36:01 +0330

    imptove -Implemented the first occurrence algorithm with better time complexity, O(log n).

--- first_occurrence.py
+++ first_occurrence.py
@@ -8,3 +8,22 @@ def first_occurrence(array: list[int], target: int) -> int:
 
     return -1
 
+def first_occurrence_logn(array: list[int], target: int) -> int:
+    """Implement with better time complexity, O(log n)
+    """
+    low, high = 0, len(array) - 1
+
+    while low <= high:
+        mid = (high + low) // 2
+
+        if low == high:
+            break
+        elif array[mid] < target:
+            low = mid + 1
+        else:
+            high = mid
+
+    if array[low] == target:
+        return low
+    return -1
+
