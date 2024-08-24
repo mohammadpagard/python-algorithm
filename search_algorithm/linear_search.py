e8bbce074ead3e6d7c8a5ecaeb08fe210da7b809
richie.p <mohammadpagard.dev@gmail.com>
2024-08-24 20:22:15 +0330

    improve -Wrote the test for linear search algorithm.

--- search_algorithm/linear_search.py
+++ search_algorithm/linear_search.py
@@ -1,7 +1,6 @@
 def linear_search(array: list[int], target: int) -> str:
     for i in range(len(array)):
         if array[i] == target:
-            return "The {} is in the {} index of the array".format(target, i)
+            return "The {} is in the {} index of the array.".format(target, i)
 
-    return "Your target is not in the array..."
-
+    return "Your target isn't in the array."
